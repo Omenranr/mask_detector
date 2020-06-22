bde85acdfe652a9009215068b89780bf40633acc
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-06-22 16:01:04 +0100

    remove tf

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-
 import numpy as np
 import argparse
 import cv2
@@ -10,12 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def showHello():
     return "hello world"
-@app.route('/classifyMask', methods=['POST'])
-def results():
-    form = request.form
-    if request.method == 'POST':
-        print("test")
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
