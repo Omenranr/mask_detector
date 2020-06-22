75790f312c3725c38383e5b8c8ccbd557f40321b
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-06-22 16:42:43 +0100

    add tf imports

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from flask import Flask 
-  
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+
 app = Flask(__name__) 
   
 @app.route("/") 
